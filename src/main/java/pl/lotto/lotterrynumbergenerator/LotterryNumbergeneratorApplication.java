6e1b4d977113d90ec14ec5894eddb81ccb22d476
TomaszSiarkiewicz <TomaszSiarkiewicz@gmial.com>
2023-04-17 10:37:13 +0200

    Initial commit

---
+++ src/main/java/pl/lotto/lotterrynumbergenerator/LotterryNumbergeneratorApplication.java
@@ -0,0 +1,13 @@
+package pl.lotto.lotterrynumbergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotterryNumbergeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotterryNumbergeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lotto/lotterrynumbergenerator/LotterryNumbergeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lotto.lotterrynumbergenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotterryNumbergeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
